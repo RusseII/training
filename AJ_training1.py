752711807039eca1652dc5e39303ed5a14d4b2d6
AJ <ajs262@zips.uakron.edu>
2016-11-29 12:26:44 -0500

    AJ's first commit

---
+++ AJ_training1.py
@@ -0,0 +1,10 @@
+# for i in range (50,100):
+#	print i 
+myArray = [5,22,18,53,10]
+for items in myArray:
+	print items
+
+newArray=sorted(myArray)
+print newArray
+for donger in newArray:
+	print donger
